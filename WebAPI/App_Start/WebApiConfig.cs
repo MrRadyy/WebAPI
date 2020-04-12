75817add62ebbbb78c35a51c1de2cf4bfdd20544
radekperina61 <radekperina61@gmail.com>
2020-04-12 20:14:12 +0200

    Functional Api

--- WebAPI/App_Start/WebApiConfig.cs
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebAPI
 {
@@ -23,6 +24,7 @@ namespace WebAPI
                 );
 
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.EnableCors(new EnableCorsAttribute( "http://localhost:4200", "*", "*")); 
         }
     }
 }
